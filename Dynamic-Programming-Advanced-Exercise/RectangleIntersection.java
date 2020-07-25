b487f102c9733e5d2f5a3c91632c9876383ba140
Ivailo97 <ivailo.8.1993@abv.bg>
2020-07-25 14:56:38 +0300

    update

--- Dynamic-Programming-Advanced-Exercise/RectangleIntersection.java
+++ Dynamic-Programming-Advanced-Exercise/RectangleIntersection.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class RectangleIntersection {
 
@@ -13,73 +11,33 @@ public class RectangleIntersection {
 
         int n = Integer.parseInt(reader.readLine());
 
-        List<Rectangle> rectangles = new ArrayList<>();
+        int[][] matrix = new int[2001][2001];
+        int result = 0;
 
         for (int i = 0; i < n; i++) {
 
-            int[] coordinates = Arrays.stream(reader.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            int[] coordinates = Arrays.stream(reader.readLine().split("\\s+")).mapToInt(x -> Integer.parseInt(x) + 1000).toArray();
 
-            int xMin = coordinates[0];
-            int xMax = coordinates[1];
-            int yMin = coordinates[2];
-            int yMax = coordinates[3];
+            int minX = coordinates[0];
+            int maxX = coordinates[1];
+            int minY = coordinates[2];
+            int maxY = coordinates[3];
 
-            Rectangle rectangle = new Rectangle(xMin, yMin, xMax, yMax);
-
-            rectangles.add(rectangle);
-        }
-
-        boolean[][] used = new boolean[rectangles.size()][rectangles.size()];
-
-        int totalArea = 0;
-
-        for (int i = 0; i < rectangles.size(); i++) {
-            Rectangle first = rectangles.get(i);
-
-            for (int j = 0; j < rectangles.size(); j++) {
-                Rectangle second = rectangles.get(j);
-
-                if (i == j) {
-                    continue;
-                }
-
-                if (!used[i][j] && !used[j][i]) {
-                    totalArea += overlappingArea(first, second);
-                    used[i][j] = true;
-                    used[j][i] = true;
+            for (int x = minX; x < maxX; x++) {
+                for (int y = minY; y < maxY; y++) {
+                    matrix[y][x] += 1;
                 }
             }
         }
 
-        System.out.println(totalArea);
-    }
-
-    static class Rectangle {
-
-        int xMin;
-        int yMin;
-        int xMax;
-        int yMax;
-
-        public Rectangle(int xMin, int yMin, int xMax, int yMax) {
-            this.xMin = xMin;
-            this.yMin = yMin;
-            this.xMax = xMax;
-            this.yMax = yMax;
-        }
-    }
-
-    static int overlappingArea(Rectangle a, Rectangle b) {
-
-        int area = 0;
-
-        int dx = Math.min(a.xMax, b.xMax) - Math.max(a.xMin, b.xMin);
-        int dy = Math.min(a.yMax, b.yMax) - Math.max(a.yMin, b.yMin);
-
-        if (dx >= 0 && dy >= 0) {
-            area = dx * dy;
+        for (int[] row : matrix) {
+            for (int col : row) {
+                if (col > 1) {
+                    result++;
+                }
+            }
         }
 
-        return area;
+        System.out.println(result);
     }
 }
